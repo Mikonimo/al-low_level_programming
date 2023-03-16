54d54d71825fa44e3ae1c1239aecf238dbb15407
Mark Manani <mikonani413@gmail.com>
2023-03-16 11:58:39 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - runs the statements in it
+ *
+ * Return: 0 to exit the function
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is apiece of art,");
+return (0);
+}
