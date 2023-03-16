09da3c846d254f2e88d99ccf9b6e8b01d3fdb9f6
Mark Manani <mikonani413@gmail.com>
2023-03-16 11:30:52 -0700

    firat coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main -it runs the statement inside it
+ * return 0 to exit the function
+ */
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle\n");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
