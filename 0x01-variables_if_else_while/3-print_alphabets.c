a782408608e13cc649d77e409fb0f2584b23263a
Mark Manani <mikonani413@gmail.com>
2023-03-17 06:10:21 -0700

    while2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  *
+  * Return: 0 exit the function
+  */
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+        }
+	putchar('\n');
+	return (0);
+}
