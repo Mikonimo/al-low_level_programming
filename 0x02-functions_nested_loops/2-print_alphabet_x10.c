cea850155f4fe7e20a766b24b484ea7d1599a5c7
Mark Manani <mikonani413@gmail.com>
2023-03-21 06:08:58 -0700

    function2'

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - print the alphabet x10
+  *
+  * Return: 0 success
+  */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
