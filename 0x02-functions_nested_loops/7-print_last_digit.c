a76680a727f83b51527618bf8de5e5030e68c376
Mark Manani <mikonani413@gmail.com>
2023-03-21 14:08:20 -0700

    function8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,23 +9,8 @@ int print_last_digit(int x)
 {
 	int r = x % 10;
 
-	_abs(x);
+	if (r < 0)
+		r *= -1;
 	_putchar(r + '0');
 	return (r);
-}
-/**
-  * _abs - calculate the absolute value of an integer
-  * @i: thre value being computed
-  * Return: 0 success
-  */
-int _abs(int i)
-{
-	if (i < 0)
-	{
-		return (-i);
-	}
-	else
-	{
-		return (i);
-	}
 }
