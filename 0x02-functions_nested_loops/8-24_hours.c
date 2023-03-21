5891bf2d2291d8c980f45b36280c4a9b44f45315
Mark Manani <mikonani413@gmail.com>
2023-03-21 09:51:43 -0700

    functions9

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,16 +5,18 @@
   */
 void jack_bauer(void)
 {
-	for (int hr1 = 0; hr1 <= 2; hr1++)
+	int hr1, hr2, min1, min2;
+
+	for (hr1 = 0; hr1 <= 2; hr1++)
 	{
-		for (int hr2 = 0; hr2 <= 9; hr2++)
+		for (hr2 = 0; hr2 <= 9; hr2++)
 		{
 			if (hr1 == 2 && hr2 == 4)
 				break;
 
-			for (int min1 = 0; min1 <= 5; min1++)
+			for (min1 = 0; min1 <= 5; min1++)
 			{
-				for (int min2 = 0; min2 <= 9; min2++)
+				for (min2 = 0; min2 <= 9; min2++)
 				{
 					_putchar(hr1 + '0');
 					_putchar(hr2 + '0');
