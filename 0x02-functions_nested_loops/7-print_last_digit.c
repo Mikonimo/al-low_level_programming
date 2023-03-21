8fd17e270c6c8395c4234b537aa29bb59e0e989c
Mark Manani <mikonani413@gmail.com>
2023-03-21 08:27:35 -0700

    function8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * print_last_digit - prints the last digit of a no
+  * @x: the no being evaluated
+  *
+  * Return: the last digit
+  */
+int print_last_digit(int x)
+{
+	int r = x % 10;
+
+	return (r);
+}
