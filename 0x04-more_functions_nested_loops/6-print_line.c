19db651ea5e80601576e3268239995caac28e8b3
Mark Manani <mikonani413@gmail.com>
2023-03-23 08:59:16 -0700

    new line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_line - prints a straight line in the terminal
+  * @n: numbe of times to be printed
+  *
+  */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}	
+	
