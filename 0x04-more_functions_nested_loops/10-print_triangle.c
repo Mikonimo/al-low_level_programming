8df2ab7f09d225e823b860aec32353fdad532c66
Mark Manani <mikonani413@gmail.com>
2023-03-23 14:26:18 -0700

    task 8

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,20 +1,11 @@
-#include "main.h"
-/**
-  * print_triangle - prints a triangle
-  * @size: size to be drawn
-  *
-  */
-void print_triangle(int size)
-{
-	if (size <= 0)
-	{
+void print_triangle(int size) {
+	if (size <= 0) {
 		_putchar('\n');
 		return;
 	}
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j <= i; j++)
-		{
+
+	for (int row = 1; row <= size; row++) {
+		for (int col = 1; col <= row; col++) {
 			_putchar('#');
 		}
 		_putchar('\n');
