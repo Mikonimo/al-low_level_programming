36f711e32d54918860328ecd5f0f672a0edb690a
Mark Manani <mikonani413@gmail.com>
2023-03-29 12:43:49 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s: the string being capitalized
+ *
+ * Return: capitalized string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
+			    s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			    s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '\"' ||
+			    s[i - 1] == '(' || s[i - 1] == ')' ||
+			    s[i - 1] == '{' || s[i - 1] == '}')
+			{
+				s[i] -= 32;
+			}
+		}
+	}
+
+	return (s);
+}
