22b138cc8baabda9e94fad49bfcc57ce36364be7
Mark Manani <mikonani413@gmail.com>
2023-03-30 09:46:22 -0700

    Task advanced

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,4 @@
-#include :main.h"
+#include "main.h"
 /**
  * *rot13 - encodes a string using rot13
  * @s: the string being encoded
@@ -7,4 +7,23 @@
  */
 char *rot13(char *s)
 {
-	
+	char *p = s;
+	int i, j;
+	char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			j = s[i] - 'a';
+			s[i] = rot[j];
+		}
+		else if (s[i] >= 'A' && s[i] <= 'Z')
+		{
+			j = s[i] - 'A';
+			s[i] = rot[j + 26];
+		}
+	}
+
+	return (p);
+}
