69ca74b7123ce38cec7fd363794a73da971ae22e
Mark Manani <mikonani413@gmail.com>
2023-04-03 08:21:00 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset - fills the first n bytes of memory area pointed by s with a constant byte b
+ * _memset - fills memory with a constant byte
  * @s: the string being evaluated
  * @b: the constant byte to be inserted
  * @n: the no of bytes being replaced
@@ -9,17 +9,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
 	unsigned int x = 0;
-
-	while (s[i] != '\0')
+	
+	while (x < n)
 	{
-		while (x < n)
-		{
-			s[i] = b;
-			x++;
-		}
-		i++;
+		s[x] = b;
+		x++;
 	}
+
 	return (s);
 }
