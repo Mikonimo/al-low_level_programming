d2b51f58bea95fd814a0991ccd3c3141a7823f11
Mark Manani <mikonani413@gmail.com>
2023-04-09 07:59:13 -0700

    Static Libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,30 @@
-int _atoi(char *s);
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string being converted
+ *
+ * Return: the integer or 0
+ */
+int _atoi(char *s)
+{
+	int sn = 1;
+	int n = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			sn *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			n = n * 10 + (*s - '0');
+		}
+		else if (n > 0)
+		{
+			break;
+		}
+		s++;
+	}
+	return (sn * n);
+}
