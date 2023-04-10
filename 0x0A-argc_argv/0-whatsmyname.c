ba5c532a08e3a418349ec8c28497cf04548ca9a6
Mark Manani <mikonani413@gmail.com>
2023-04-10 16:51:40 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point to a program
+ * @argc: no of arguments supplied to the program
+ * @argv: array of pointers to the strings
+ *
+ * Return: 0 success otherwise failure
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Learning about argumens used i function main
