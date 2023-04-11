f15c7300f40c89e796efd5d4bc8a15459e88d9bb
Mark Manani <mikonani413@gmail.com>
2023-04-10 17:04:56 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - enty point
+ * @argc: no of arguments
+ * @argv: array of stringd=s
+ *
+ * Return: 0 success, otherwise failure
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 0)
+	{
+	}
+	printf("%d\n", argc - 1);
+	return (0);
+}
