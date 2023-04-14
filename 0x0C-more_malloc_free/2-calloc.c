74bcddbc9e4f1cd638dcb74c7d1255a6333a0f68
Mark Manani <mikonani413@gmail.com>
2023-04-14 02:33:36 -0700

    first commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
-		return( NULL);
+		return(NULL);
 
 	char_ptr = ptr;
 	for (i = 0; i < nmemb * size; i++)
