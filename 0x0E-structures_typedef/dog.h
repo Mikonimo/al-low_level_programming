84476c573e1906392ab96d0516f81b5205490f9f
Mark Manani <mikonani413@gmail.com>
2023-04-18 04:55:54 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,27 @@
-#ifndef
-DOG_H
-#define
-DOG_H
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - structure for dog
  * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
  *
+ *Description: it contains information about a dog
  */
-struct dog
+struct
+
+dog
+
+
 {
-	char *name;
-	float age;
-	char *owner;
+
+char
+	*name;
+
+float
+	age;
+
+char
+	*owner;
 };
 #endif
