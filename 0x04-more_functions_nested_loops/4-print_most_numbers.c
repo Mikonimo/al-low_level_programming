d7f912353321cfc22ce50a861dbb09e7736811c1
Mark Manani <mikonani413@gmail.com>
2023-05-11 02:34:33 -0700

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,13 @@
   */
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	while (i >= 0 && i <= 9)
+	for (i = 0; i >= 0 && i <= 9; i++)
 	{
 		if (i == 2 || i == 4)
 			continue;
 		_putchar(i + '0');
-		i++;
 	}
 	_putchar('\n');
 }
