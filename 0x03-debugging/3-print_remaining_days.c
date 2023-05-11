aec4e635d401c85ced1ad0c2c0febf1f153ae753
Mark Manani <mikonani413@gmail.com>
2023-05-11 01:49:19 -0700

    update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,22 +12,27 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	int days_in_feb = 28;
+    if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
+    {
+        if (month >= 2 && day >= 60)
+        {
+            day++;
+        }
 
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-	{
-		days_in_feb = 29;
-	}
-
-	if (month == 2 && day > days_in_feb)
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-	}
-	else
-	{
-		int day_of_year = convert_day(month, day);
-
-		printf("Day of the year: %d\n", day_of_year);
-		printf("Remaining days: %d\n", 365 + (days_in_feb == 29) - day_of_year);
-	}
+        printf("Day of the year: %d\n", day);
+        printf("Remaining days: %d\n", 366 - day);
+    }
+    else
+    {
+        if (month == 2 && day == 60)
+        {
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+        }
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 365 - day);
+        }
+    }
 }
+
