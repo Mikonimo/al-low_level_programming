5393ef30943ac452d053232e69e491e482174596
Mark Manani <mikonani413@gmail.com>
2023-05-13 08:31:27 -0700

    Update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,17 +1,28 @@
-void print_triangle(int size) {
-	if (size <= 0) {
+#include "main.h"
+/**
+ * print_triangle - prints a triangle followed by a new line
+ * @size: the size of the triangle
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
 		_putchar('\n');
 		return;
 	}
-
-
-int row;
-int col;
-
-for  (row = 1; row <= size; row++) {
-for  (col = 1; col <= row; col++) {
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
 			_putchar('#');
-
+		}
 		_putchar('\n');
 	}
 }
