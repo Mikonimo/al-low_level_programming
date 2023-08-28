92c8b2b781c4cd6026027dab3fcd91a76a839b37
Mark Manani <mikonani413@gmail.com>
2023-08-28 18:10:27 +0300

    sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 	int n = 1;
-	int result;
+	int result = 0;
 
 	while (n < 1024)
 	{
 		if (n % 3 == 0 || n % 5 == 0)
-			result =+ n;
+			result = result + n;
 		n++;
 	}
 	printf("%d\n", result);
-	return(0);
+	return (0);
 }
