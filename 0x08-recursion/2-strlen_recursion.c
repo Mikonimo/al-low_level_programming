1665f560bdb835a3d3917072f6e921c56044c95e
Mark Manani <mikonani413@gmail.com>
2023-08-30 17:10:00 +0300

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string being evaluated
+ *
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s == '\0')
+		return;
+	length++;
+	_strlen_recursion(s + 1);
+	return (length);
+}
