319384d5878faaedf3c99a2fd6db8679d3c28b78
Mark Manani <mikonani413@gmail.com>
2023-09-01 23:41:54 +0300

    Task_2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc - count
+ * @argv - arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int n;
+
+	for (n = 0; n < argc; n++)
+		printf("%s\n", argv[n]);
+	return (0);
+}
