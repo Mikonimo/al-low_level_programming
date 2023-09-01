cd02b2eead25f1e6ce31eb74733378c3f755dd6c
Mark Manani <mikonani413@gmail.com>
2023-09-02 00:44:23 +0300

    Task_4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: count
+ * @argv: arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int n, i;
+	int result = 0;
+
+	if (argc == 1)
+		printf("0\n");
+	for (n = 1; n < argc; n++)
+	{
+		i = atoi(argv[n]);
+		if (i > 0 )
+			result += i;
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
