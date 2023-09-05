9e51a41ca4b9453bff278747a6937e73332dd212
Mark Manani <mikonani413@gmail.com>
2023-09-05 16:52:00 +0300

    update

---
+++ 0x0B-malloc_free/0-create_repository.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars
+ * @size: the size of the array
+ * @c: the initializing character of the array
+ *
+ * Return: the pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int n;
+
+	if (size == 0)
+		return (NULL);
+	char *arr = malloc(sizeof(char) * size);
+	if (arr == NULL)
+		return (NULL);
+	for (n = 0; n < size; n++)
+		arr[0] == c;
+	return (arr);
+}
