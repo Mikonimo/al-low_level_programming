256bfdf8e5ba43ce851aa92315ce3dba3ef8840c
Mark Manani <mikonani413@gmail.com>
2023-09-12 18:13:39 +0300

    Task_1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - initializes a struct dog
+ * @d: Pointer to the struct dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		dd->age = age;
+		d->owner = owner;
+	}
+}
