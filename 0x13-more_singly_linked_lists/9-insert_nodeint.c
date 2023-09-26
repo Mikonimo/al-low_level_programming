a78113f555e77ba432c92a606d7cc8999ec33452
Mikonimo <mikonani413@gmail.com>
2023-09-27 01:53:14 +0300

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prev;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
 	node0 = malloc(sizeof(listint_t));
 	if (node0 == NULL)
 		return (NULL);
