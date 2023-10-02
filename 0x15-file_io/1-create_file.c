e8ba6276ae58e747a4d08541b8043a46ce74f9bc
Mikonimo <mikonani413@gmail.com>
2023-10-02 19:26:25 +0300

    Task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	op = open(filename, O_RDWR | O_CREAT);
+	op = open(filename, O_CREAT | O_RDWR | O_EXCL, S_IRUSR | S_IWUSR);
 	if (op < 0)
 		return (-1);
 	if (text_content != NULL)
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success els -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int op, wr;
+
+	if (filename == NULL)
+		return (-1);
+	op = open(filename, O_APPEND);
+	if (op < 0)
+		return (-1);
+	wr = write(op, text_content, strlen(text_content));
+	if (wr < 0)
+		return (-1);
+	return (1);
+}
+
