164050e2a41c1194040c6a4195f362fb4df69cb9
Mikonimo <mikonani413@gmail.com>
2023-10-02 21:11:18 +0300

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	op = open(filename, O_APPEND);
+	op = open(filename, O_WRONLY | O_APPEND);
 	if (op < 0)
 		return (-1);
 	if (text_content != NULL)
