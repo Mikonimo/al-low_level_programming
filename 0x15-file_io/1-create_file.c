6d9de902c89c27d4747e1e4716e61f993b191f7a
Mikonimo <mikonani413@gmail.com>
2023-10-02 21:03:05 +0300

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,14 +16,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	op = open(filename, O_CREAT | O_RDWR | O_EXCL, S_IRUSR | S_IWUSR);
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (op < 0)
 		return (-1);
 	if (text_content != NULL)
 	{
 		wr = write(op, text_content, strlen(text_content));
 		if (wr < 0)
+		{
+			close(op);
 			return (-1);
+		}
 	}
 	close(op);
 	return (1);
