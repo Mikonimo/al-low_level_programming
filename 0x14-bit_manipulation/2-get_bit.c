361d3ecb95b308b82e2ef661f1fa6074db72d58c
Mikonimo <mikonani413@gmail.com>
2024-02-08 19:19:02 +0300

    push

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * get_bit - returns the value of a bit given index
+ * @n: the no being evaluated
+ * @index: the position of the bit you want to get
+ *
+ * Return: value of a bit at a given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1UL << index;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	if ((n & mask) == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: pointer to the no being evaluated
+ * @index: the index of the bit to set
+ *
+ * Return: 1 if successful else -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1UL << index;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	*n = *n | mask;
+	return (1);
+}
