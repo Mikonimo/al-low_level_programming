51afe31a82592166d8d6569dd49e9f5415ef4236
Mikonimo <mikonani413@gmail.com>
2024-03-21 18:33:43 +0300

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	node->next = NULL;
 	if (idx == 0)
 	{
-		add_dnodeint(curr, node->n)
+		add_dnodeint(curr, node->n);
 	}
 	while (i < idx - 1 && curr != NULL)
 	{
