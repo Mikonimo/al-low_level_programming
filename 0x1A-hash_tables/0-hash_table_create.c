4eae7a1f192795d2f6576629dbfb79e1524945d4
Mikonimo <mikonani413@gmail.com>
2024-04-19 01:16:24 +0300

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,12 +7,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	hash_table_t *table;
 	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
+	{
 		return (NULL);
+	}
 	table->array = malloc(sizeof(hash_node_t) * size);
 	if (table->array == NULL)
 	{
